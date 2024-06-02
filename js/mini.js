de550c82f979d52497689f0a88859a9d4ff17e7b
TamangLhoSantosh <sureshtama.g12@gmail.com>
2024-06-02 20:41:37 +0545

    closure

--- js/mini.js
+++ js/mini.js
@@ -1,23 +1,23 @@
-function fetchData() {
-  let promise = new Promise((resolve) => {
-    setTimeout(() => {
-      const data = [
-        { id: 1, name: "John", age: 40 },
-        { id: 2, name: "Harry", age: 65 },
-        { id: 3, name: "Arya", age: 56 },
-      ];
-      resolve(data);
-    }, 2000);
-  });
-  return promise;
-}
+// function fetchData() {
+//   let promise = new Promise((resolve) => {
+//     setTimeout(() => {
+//       const data = [
+//         { id: 1, name: "John", age: 40 },
+//         { id: 2, name: "Harry", age: 65 },
+//         { id: 3, name: "Arya", age: 56 },
+//       ];
+//       resolve(data);
+//     }, 2000);
+//   });
+//   return promise;
+// }
 
-async function getData() {
-  const data = await fetchData();
-  console.log(data);
-}
+// async function getData() {
+//   const data = await fetchData();
+//   console.log(data);
+// }
 
-getData();
+// getData();
 
 // function createCounter() {
 //   let count = 0;
@@ -30,3 +30,10 @@ getData();
 //     },
 //   };
 // }
+
+// const counter = createCounter();
+
+// counter.increment();
+// counter.increment();
+// counter.increment();
+// console.log(counter.getCount());
