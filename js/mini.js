9fc3ab5e3adade21b303a3d8754932bdaef6d4f1
TamangLhoSantosh <sureshtama.g12@gmail.com>
2024-06-02 21:13:33 +0545

    object manipulation

--- js/mini.js
+++ js/mini.js
@@ -84,3 +84,33 @@
 // }
 
 // console.log(filterArray([1, 2, 3, 4, 5, 10, 20, 30]));
+
+// 7.Find
+// function findNumber(array) {
+//   return array.find((number) => number > 15);
+// }
+
+// console.log(findNumber([1, 2, 3, 4, 5, 10, 20, 30]));
+
+// 8. Reduce
+// function sumArray(array) {
+//   return array.reduce((acc, curr) => acc + curr, 0);
+// }
+
+// console.log(sumArray([1, 2, 3, 4, 5]));
+
+// 9. Object Manipulation
+function transform(array) {
+  return array.reduce((acc, curr) => {
+    acc[curr.id] = curr;
+    return acc;
+  }, {});
+}
+
+const users = [
+  { id: 1, name: "John", age: 40 },
+  { id: 2, name: "Harry", age: 65 },
+  { id: 3, name: "Arya", age: 56 },
+];
+
+console.log(transform(users));
